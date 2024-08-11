f1feadf7902b31efeb7a17dc9d2dec81e7f910fe
tomsains <thomas.sainsbury@epfl.ch>
2024-08-11 11:07:20 +0200

    added a new script for stock price prediciton

---
+++ LSTM_stock_price_2.py
@@ -0,0 +1,139 @@
+
+import numpy as np 
+import pandas as pd 
+import yfinance as yf
+import os
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+import torch
+import torch.nn as nn
+
+
+ticker = 'TSLA'
+data = yf.download(ticker, start='2010-01-01', end='2023-01-01')
+
+date = data.index
+data = data[['Close']]
+data ["date"] =np.array(date)
+data.reset_index()
+
+print(data.head(5))
+
+
+
+
+plt.figure(figsize = (15,9))
+plt.plot(data[['Close']])
+
+plt.title(" Stock Price",fontsize=18, fontweight='bold')
+plt.xlabel('Date',fontsize=18)
+plt.ylabel('Close Price (USD)',fontsize=18)
+plt.show()
+
+
+
+price = data[['Close']]
+scaler = MinMaxScaler(feature_range=(-1, 1))
+price['Close'] = scaler.fit_transform(price['Close'].values.reshape(-1,1))
+
+
+def split_data(stock, lookback):
+    data_raw = stock.to_numpy() # convert to numpy array
+    data = []
+    
+    # create all possible sequences of length seq_len
+    for index in range(len(data_raw) - lookback): 
+        data.append(data_raw[index: index + lookback])
+    
+    data = np.array(data)
+    test_set_size = int(np.round(0.2*data.shape[0]))
+    train_set_size = data.shape[0] - (test_set_size)
+    
+    x_train = data[:train_set_size,:-1,:]
+    y_train = data[:train_set_size,-1,:]
+    
+    x_test = data[train_set_size:,:-1]
+    y_test = data[train_set_size:,-1,:]
+    
+    return [x_train, y_train, x_test, y_test]
+
+
+lookback = 20 # choose sequence length
+x_train, y_train, x_test, y_test = split_data(price, lookback)
+print('x_train.shape = ',x_train.shape)
+print('y_train.shape = ',y_train.shape)
+print('x_test.shape = ',x_test.shape)
+print('y_test.shape = ',y_test.shape)
+
+
+
+x_train = torch.from_numpy(x_train).type(torch.Tensor)
+x_test = torch.from_numpy(x_test).type(torch.Tensor)
+y_train_lstm = torch.from_numpy(y_train).type(torch.Tensor)
+y_test_lstm = torch.from_numpy(y_test).type(torch.Tensor)
+y_train_gru = torch.from_numpy(y_train).type(torch.Tensor)
+y_test_gru = torch.from_numpy(y_test).type(torch.Tensor)
+
+class LSTM(nn.Module):
+    def __init__(self, input_dim, hidden_dim, num_layers, output_dim):
+        super(LSTM, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        
+        self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_()
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_()
+        out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
+        out = self.fc(out[:, -1, :]) 
+        return out
+    
+    
+input_dim = 1
+hidden_dim = 32
+num_layers = 2
+output_dim = 1
+num_epochs = 1000
+
+model = LSTM(input_dim=input_dim, hidden_dim=hidden_dim, output_dim=output_dim, num_layers=num_layers)
+criterion = torch.nn.MSELoss(reduction='mean')
+optimiser = torch.optim.Adam(model.parameters(), lr=0.01)
+
+
+import time
+
+hist = np.zeros(num_epochs)
+start_time = time.time()
+lstm = []
+
+for t in range(num_epochs):
+    y_train_pred = model(x_train)
+
+    loss = criterion(y_train_pred, y_train_lstm)
+    print("Epoch ", t, "MSE: ", loss.item())
+    hist[t] = loss.item()
+
+    optimiser.zero_grad()
+    loss.backward()
+    optimiser.step()
+    
+training_time = time.time()-start_time
+print("Training time: {}".format(training_time))
+
+
+predict = pd.DataFrame(scaler.inverse_transform(y_train_pred.detach().numpy()))
+original = pd.DataFrame(scaler.inverse_transform(y_train_lstm.detach().numpy()))
+
+
+
+fig, ax = plt.subplots(1, 2)
+ax[0].plot(original.index, original[0])
+ax[0].plot( predict.index,  predict[0])
+ax[0].set_title('Stock price', size = 14, fontweight='bold')
+ax[0].set_xlabel("Days", size = 14)
+ax[0].set_ylabel("Cost (USD)", size = 14)
+ax[0].set_xticklabels('', size=10)
+plt.show()
+
